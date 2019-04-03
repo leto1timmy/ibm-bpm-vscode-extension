8bbdb64fde3845d370c4cea25ca1e26e8070d100
Marek Michalcewicz <marek.michalcewicz@gmail.com>
2019-04-03 10:18:18 +0200

    json added

--- src/extension.ts
+++ src/extension.ts
@@ -4,87 +4,77 @@ import * as vscode from 'vscode';
 
 import BPMCompletionItemProvider from './features/completionItemProvider';
 
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 
-	let provider1 = vscode.languages.registerCompletionItemProvider('javascript', {
-		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-
-			// a simple completion item which inserts `Hello World!`
-			const simpleCompletion = new vscode.CompletionItem('Hello World!');
+	// let provider1 = vscode.languages.registerCompletionItemProvider('javascript', {
+	// 	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
 			// a completion item that inserts its text as snippet,
 			// the `insertText`-property is a `SnippetString` which we will
 			// honored by the editor.
-			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
+			// const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+			// snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+			// snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
 
 			// a completion item that can be accepted by a commit character,
 			// the `commitCharacters`-property is set which means that the completion will
 			// be inserted and then the character will be typed.
-			const commitCharacterCompletion = new vscode.CompletionItem('console');
-			commitCharacterCompletion.commitCharacters = ['.'];
-			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+			// const commitCharacterCompletion = new vscode.CompletionItem('console');
+			// commitCharacterCompletion.commitCharacters = ['.'];
+			// commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
 
 			// a completion item that retriggers IntelliSense when being accepted,
 			// the `command`-property is set which the editor will execute after 
 			// completion has been inserted. Also, the `insertText` is set so that 
 			// a space is inserted after `new`
-			const commandCompletion = new vscode.CompletionItem('new');
-			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-			commandCompletion.insertText = 'new ';
-			commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+			// const commandCompletion = new vscode.CompletionItem('new');
+			// commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+			// commandCompletion.insertText = 'new ';
+			// commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
 
-			const twCharacterCompletion = new vscode.CompletionItem('tw');
-			commitCharacterCompletion.commitCharacters = ['.'];
-			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `IBM BPM namespaces.`');
+	// 		const twCharacterCompletion = new vscode.CompletionItem('tw');
+	// 		twCharacterCompletion.commitCharacters = ['.'];
+	// 		twCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `IBM BPM namespaces.`');
 
-			// return all completion items as array
-			return [
-				twCharacterCompletion,
-				simpleCompletion,
-				snippetCompletion,
-				commitCharacterCompletion,
-				commandCompletion
-			];
-		}
-	});
+	// 		// return all completion items as array
+	// 		return [
+	// 			// simpleCompletion,
+	// 			// snippetCompletion,
+	// 			// commitCharacterCompletion,
+	// 			// commandCompletion,
+	// 			twCharacterCompletion
+	// 		];
+	// 	}
+	// });
 
-	const provider2 = vscode.languages.registerCompletionItemProvider('javascript', {
-			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-
-				// get all text until the `position` and check if it reads `console.`
-				// and iff so then complete if `log`, `warn`, and `error`
-				let linePrefix = document.lineAt(position).text.substr(0, position.character);
-				if (!linePrefix.endsWith('tw.')) {
-					return undefined;
-				}
-
-				return [
-					new vscode.CompletionItem('local', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('system', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('object', vscode.CompletionItemKind.Method),
-				];
-			}
-		},
-		'.' // triggered whenever a '.' is being typed
-	);
+	// const provider2 = vscode.languages.registerCompletionItemProvider('javascript', {
+	// 	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+	// 		let linePrefix = document.lineAt(position).text.substr(0, position.character);
+	// 		if (!linePrefix.endsWith('tw.')) {
+	// 			return undefined;
+	// 		}
+	// 	}
+	// },
+	// 	'.' // triggered whenever a '.' is being typed
+	// );
 
 
-	context.subscriptions.push(provider1, provider2);
 	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('javascript', new BPMCompletionItemProvider(), '.'));
+	//context.subscriptions.push(provider1, provider2);
+
 	console.log('Congratulations, your extension "ibm-bpm-vscode-extension" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	// let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
+	// Display a message box to the user
 	// 	vscode.window.showInformationMessage('Hello World!');
 	// });
 
@@ -92,4 +82,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
--- src/features/completionItemProvider.ts
+++ src/features/completionItemProvider.ts
@@ -1,50 +1,111 @@
 import { CompletionItemProvider, CompletionItem, CompletionItemKind, CancellationToken, TextDocument, Position, Range, TextEdit, workspace, CompletionContext } from 'vscode';
 import twSystemFunctions = require('./ibm-bpm-api/tw-system/systemFunctions');
 import twSystemVariables = require('./ibm-bpm-api/tw-system/systemVariables');
+let fs = require('fs');
+export interface IEntry { description?: string; signature?: string; }
+export interface IEntries { [name: string]: IEntry; }
+//import * as twSystemJSON from './ibm-bpm-api/tw-system/tw-system.json';
+
 
 export default class BPMCompletionItemProvider implements CompletionItemProvider {
     provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken, context: CompletionContext): Promise<CompletionItem[]> {
-        let result: CompletionItem[] = [];
 
+        let result: CompletionItem[] = [];
+        let json = JSON.parse(fs.readFileSync('/Users/marekmichalcewicz/ibm-bpm-vscode-extension/src/features/ibm-bpm-api/tw-system/tw-system.json', 'utf8'));
+        console.log(json);
         // let shouldProvideCompletionItems = workspace.getConfiguration('javascript').get<boolean>('suggest.basic', true);
         // if (!shouldProvideCompletionItems) {
         // 	return Promise.resolve(result);
         // }
 
         let linePrefix = document.lineAt(position).text.substr(0, position.character);
-        if (!linePrefix.endsWith('tw.system.')) {
-            return Promise.resolve(result);
-        }
+        // if (!linePrefix.endsWith('tw.system.')) {
+        //     return Promise.resolve(result);
+        // }
 
-        let added: any = {};
-        let createNewProposal = function (kind: CompletionItemKind, name: string, entry: twSystemVariables.IEntry | null): CompletionItem {
-            let proposal: CompletionItem = new CompletionItem(name);
-            proposal.kind = kind;
-            if (entry) {
-                if (entry.description) {
-                    proposal.documentation = entry.description;
+        let strArray = linePrefix.split('.');
+
+        if (strArray[strArray.length - 1].length === 0) {
+
+            console.log(strArray);
+            let jsonPath = "json";
+            for (let index in strArray) {
+                if (strArray[index].length === 0) {
+                    break;
                 }
-                if (entry.signature) {
-                    proposal.detail = entry.signature;
+                console.log(index);
+                console.log(strArray[index]);
+                // try {json = json.strArray[index];} catch(e) {
+                //     console.log(e);
+                // }
+                jsonPath = jsonPath + ("." + strArray[index]);
+
+
+                console.log(jsonPath);
+
+            }
+
+
+
+
+            //let added: any = {};
+            let createNewProposal = function (kind: CompletionItemKind, name: string, entry: twSystemVariables.IEntry | null): CompletionItem {
+                let proposal: CompletionItem = new CompletionItem(name);
+                proposal.kind = kind;
+                if (entry) {
+                    if (entry.description) {
+                        proposal.documentation = entry.description;
+                    }
+                    if (entry.signature) {
+                        proposal.detail = entry.signature;
+                    }
                 }
-            }
-            return proposal;
-        };
+                return proposal;
+            };
 
-        for (let systemFunctions in twSystemFunctions.systemFunctions) {
-            if (twSystemFunctions.systemFunctions.hasOwnProperty(systemFunctions)) {
-                added[systemFunctions] = true;
-                result.push(createNewProposal(CompletionItemKind.Function, systemFunctions, twSystemFunctions.systemFunctions[systemFunctions]));
-            }
-        }
 
-        for (let systemVariables in twSystemVariables.systemVariables) {
-            if (twSystemVariables.systemVariables.hasOwnProperty(systemVariables)) {
-                added[systemVariables] = true;
-                result.push(createNewProposal(CompletionItemKind.Variable, systemVariables, twSystemVariables.systemVariables[systemVariables]));
-            }
+            let names = Object.getOwnPropertyNames(eval(jsonPath));
+            console.log(names);
+
+            names.forEach(name => {
+                let x: IEntry = {};
+                x.description = "asd";
+                x.signature = "qwe";
+                result.push(createNewProposal(CompletionItemKind.Variable, name, x));
+            });
+
+            // for (let name in names) {
+            //     let x: IEntry = {};
+            //     x.description = "asd";
+            //     x.signature = "qwe";
+            //     result.push(createNewProposal(CompletionItemKind.Variable, name, x));
+
+            // }
+
+
+            // let prepareProposals = function (linePrefix: string): CompletionItem[] {
+            //     let proposals: CompletionItem[] = [];
+
+            //     proposals = 
+            //     return proposals;
+            // };
+
+
+            // for (let systemFunctions in twSystemFunctions.systemFunctions) {
+            //     if (twSystemFunctions.systemFunctions.hasOwnProperty(systemFunctions)) {
+            //         added[systemFunctions] = true;
+            //         result.push(createNewProposal(CompletionItemKind.Function, systemFunctions, twSystemFunctions.systemFunctions[systemFunctions]));
+            //     }
+            // }
+
+            // for (let systemVariables in twSystemVariables.systemVariables) {
+            //     if (twSystemVariables.systemVariables.hasOwnProperty(systemVariables)) {
+            //         added[systemVariables] = true;
+            //         result.push(createNewProposal(CompletionItemKind.Variable, systemVariables, twSystemVariables.systemVariables[systemVariables]));
+            //     }
+            // }
         }
         console.log(result);
-        return Promise.resolve(result);
+            return Promise.resolve(result);
     }
 }
